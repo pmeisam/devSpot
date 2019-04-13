d98fdac0296c9f382f2a1109d85e2ff0a44147b4
Meisam <pmeisam@ymail.com>
2019-04-13 02:48:58 -0700

    messaging works

--- src/pages/ChatPage/ChatPage.jsx
+++ src/pages/ChatPage/ChatPage.jsx
@@ -3,6 +3,7 @@ import { TextField } from "@material-ui/core";
 import { Button } from "@material-ui/core";
 import userService from "../../utils/userService";
 import socket from "../../socket";
+import Card from "@material-ui/core/Card";
 
 class ChatPage extends Component {
   state = {
@@ -13,8 +14,8 @@ class ChatPage extends Component {
     userName: null
   };
   scrollToBottom = () => {
-    this.el.scrollIntoView({ behavior: 'smooth' });
-  }
+    this.el.scrollIntoView({ behavior: "smooth" });
+  };
   handleSubmit = e => {
     e.preventDefault();
     try {
@@ -31,7 +32,7 @@ class ChatPage extends Component {
     this.setState({ [evt.target.name]: evt.target.value });
   };
   componentDidMount() {
-    this.scrollToBottom()
+    this.scrollToBottom();
     socket.registerApp(this);
     if (this.state.chat) {
       this.setState({ messages: this.state.chat.messages });
@@ -40,51 +41,60 @@ class ChatPage extends Component {
     this.setState({ user });
     this.setState({ chatId: this.props.match.params.chatId });
   }
-  componentDidUpdate () {
-    this.scrollToBottom()
+  componentDidUpdate() {
+    this.scrollToBottom();
   }
- 
+
   render() {
     console.log(this.state.messages);
     return (
       <div style={{ paddingTop: "150px" }}>
-        {this.state.messages ? 
-          this.state.messages.map(m => (
-            <p key={m._id}>
-              <span>{m.userName}:&nbsp;&nbsp;</span>
-              {m.content}
-            </p>
-          ))
-         : (
-          <p />
-        )}
-        <form onSubmit={this.handleSubmit} style={{ bottom: 0, margin: "100px auto 0 auto" }}>
-          <TextField
-            required
-            style={{ width: "70vw" }}
-            margin="normal"
-            variant="outlined"
-            type="text"
-            label="Message"
-            placeholder="Message"
-            name="message"
-            value={this.state.message}
-            onChange={this.handleChange}
-            autoComplete='off'
-          />
-          <Button
-            // onClick={this.handleSubmit}
-            disabled={this.props.message === ""}
-            size="large"
-            variant="outlined"
-            color="secondary"
-            type='submit'
-            style={{height: '57px'}}
+        <Card style={{width: '70%', margin: '0 auto', padding: '0 40px'}}>
+          {this.state.messages ? (
+            this.state.messages.map(m => (
+              <p style={{margin: '10px 40px'}} key={m._id}>
+                <span style={{fontWeight: '900'}}>{m.userName}:&nbsp;&nbsp;</span>
+                {m.content}
+              </p>
+            ))
+          ) : (
+            <p />
+          )}
+          <form
+            onSubmit={this.handleSubmit}
+            style={{ bottom: 0, margin: "100px auto 0 auto" }}
           >
-            Send
-          </Button>
-          <div ref={el => { this.el = el; }} />
-        </form>
+            <TextField
+              required
+              style={{ width: "70vw" }}
+              margin="normal"
+              variant="outlined"
+              type="text"
+              label="Message"
+              placeholder="Message"
+              name="message"
+              value={this.state.message}
+              onChange={this.handleChange}
+              autoComplete="off"
+            />
+            <Button
+              // onClick={this.handleSubmit}
+              disabled={this.props.message === ""}
+              size="large"
+              variant="outlined"
+              color="secondary"
+              type="submit"
+              style={{ height: "57px" }}
+            >
+              Send
+            </Button>
+            <div
+              ref={el => {
+                this.el = el;
+              }}
+            />
+          </form>
+        </Card>
       </div>
     );
   }
